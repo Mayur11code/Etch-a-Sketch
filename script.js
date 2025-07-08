cc8da6ced3dc2829e90feff9432c96b53a5bc00b
Mayur11code <217201393+Mayur11code@users.noreply.github.com>
2025-07-08 18:54:36 +0530

    ORGANIZED THE CODE, REFACTORED THE CUSTOM FEATURE

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// DOM MANIPULATION FOR BASIC STRUCTURE
 const box = document.createElement("div")
 const body = document.querySelector("body")
 box.setAttribute("style", "display:flex; flex-wrap:wrap; width:700px;height:700px")
@@ -5,35 +6,36 @@ body.appendChild(box)
 box.classList.add("box")
 
 
-
+// DEFAULT GRID
 let previousnumber = 16;
 grid(previousnumber);
 
-
-const button = Array.from(document.querySelectorAll("button"))
-console.log(button)
-let choice = '';
-
-
+// COMMON VARIABLES TO BE USED BY VARIOUS FUNCTIONS
+let choice;
 let count = 0;
-let dict = {};
+let dict = {};//KEEPS THE COUNT OF MOUSEOVERS PER GRIDBOX
+const button = Array.from(document.querySelectorAll("button"))
+
+
 intialize();
 
 button.forEach((item) => {
 
     item.addEventListener("click", () => {
         choice = item.getAttribute("class");
-        count = 0;
+        // count = 0;
         intialize();
+
+
         if (choice == "reset") {
             // let no = select.value || 16;
 
             // grid(+no);
-                location.reload(); //RELOADS THE PAGE [BETTER WAY]
+            location.reload(); //RELOADS THE PAGE [BETTER WAY TO RESET]
         }
 
 
-
+        //HIGHLIGHTS THE MODE IN ACTION
         button.forEach((item) => {
 
 
@@ -54,55 +56,63 @@ button.forEach((item) => {
 
 box.addEventListener("mouseover", (e) => {
 
-
-
     dict[e.target.className]++
+    console.log(dict) //PRINTS THE OBJECT FOR DUBBUGING REASONS
 
-    console.log(dict)
 
+    //SWITCH CASE NEEDED AN EXPRESSION, we are using a variable to compare "choice"
 
     if ((e.target.className.slice(0, 7)) == "gridbox" && (choice == '' || choice == "Bw")) {
-
-
         e.target.style.backgroundColor = "black";
     }
-
     else if (e.target.className.slice(0, 7) == "gridbox" && choice == ("rgb")) {
-        let red =Math.floor(Math.random()*256);
-        let blue =Math.floor(Math.random()*256);
-        let green=Math.floor(Math.random()*256);
+        let red = Math.floor(Math.random() * 256);
+        let blue = Math.floor(Math.random() * 256);
+        let green = Math.floor(Math.random() * 256);
         e.target.style.backgroundColor = `rgb(${red},${blue},${green})`;
     }
-
     else if (e.target.className.slice(0, 7) == "gridbox" && choice == "Eraser") {
-
         e.target.style.backgroundColor = "White";
     }
 
     opacity(e);
-
-
-
 })
 
-
+//Handles Drop down menu for sizes
 const select = document.querySelector("select");
 select.addEventListener("change", () => {
+    if (select.value == 'custom') {
+        document.querySelector(".custom").classList.remove("custom")
+
+    }
+    else { document.querySelector(".select").classList.add("custom") }
     let selected = select.value;
+
     grid(+selected);
     intialize();
 
 })
 
 
+//handles the input value in case of custom size
+document.querySelector(".input").addEventListener("click", () => {
+    const input = document.querySelector("input")
+    console.log(input.value);
+    let givennumber = +input.value;
+    givennumber > 50 ? alert("This is too large, it may cause lags") : 1
+    grid(givennumber);
+    intialize()
+
+})
 
 
 
 
+//*******FUNCTIONS DECLARATION CORNER *******
 
 
 
-
+//HANDLES THE OPACITY, Makes the opaccity=1 in exactly 5 steps,resets when mode changes
 function opacity(e) {
 
     let a = e.target.className.slice(0, 7);
@@ -128,7 +138,7 @@ function opacity(e) {
 }
 
 
-
+//GENERATE A GRID ON THE DRAWING BOX
 function grid(number) {
     let existance = Array.from(document.querySelectorAll(".gridbox"))
     if (existance) {
@@ -153,7 +163,8 @@ function grid(number) {
 
 
 
-// FIXING OPACITY PROBLEM
+// FIXING OPACITY PROBLEM, HELPS IN REINIITIALIZING OF OPACITY PROPERTY
+// //INITIALIZE THE HOVER COUNT STORED IN DICT [HELPS IN OPACITY PROPERTY RESET]
 function intialize() {
     const arr = Array.from(document.querySelectorAll(".gridbox"));
     arr.forEach((item) => {
